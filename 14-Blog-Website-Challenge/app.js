8c6804717c11917a40c877fb602cd9d40e14ad0a
Mohit Soni <mohitksoni@outlook.com>
2022-08-29 13:35:27 -0700

    Updating Blog Post Website by using MongoDB and Mongoose

--- 14-Blog-Website-Challenge/app.js
+++ 14-Blog-Website-Challenge/app.js
@@ -6,6 +6,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const lodash = require("lodash");
+const mongoose = require("mongoose");
 
 ////////////////////////////////
 /****** Creating the App ******/
@@ -16,6 +17,19 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+/////////////////////////////////
+/****** Setting up Server ******/
+/////////////////////////////////
+
+mongoose.connect("mongodb://localhost:27017/postsDB");
+
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Post = mongoose.model("Post", postSchema);
+
 ////////////////////////////////
 /****** Global Variables ******/
 ////////////////////////////////
@@ -23,7 +37,6 @@ app.use(express.static("public"));
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-const allPosts = [];
 
 ///////////////////////////////////
 /****** Starting the Server ******/
@@ -38,7 +51,9 @@ app.listen(3000, function() {
 //////////////////////////////
 
 app.get("/", function(req, res) {
-  res.render("home", {homeStartingContent: homeStartingContent, allPosts: allPosts});
+  Post.find({}, function(err, docs) {
+    res.render("home", {homeStartingContent: homeStartingContent, allPosts: docs});
+  });
 });
 
 app.get("/about", function(req, res) {
@@ -53,15 +68,18 @@ app.get("/compose", function(req, res) {
   res.render("compose");
 });
 
-app.get("/posts/:title", function(req, res) {
-  const requestedTitle = lodash.lowerCase(req.params.title);
+app.get("/posts/:id", function(req, res) {
+  const requestedPostId = req.params.id;
 
-  allPosts.forEach(function(post) {
-    const postTitle = lodash.lowerCase(post.title);
-
-    if (requestedTitle === postTitle) {
-      res.render("post", {postHeading: post.title, postContent: post.content});
-    }
+  Post.find({}, function(err, docs) {
+    docs.forEach(function(post) {
+      const postId = post._id.toString();
+      console.log(postId);
+  
+      if (requestedPostId === postId) {
+        res.render("post", {postHeading: post.title, postContent: post.content});
+      }
+    });
   });
 });
 
@@ -70,11 +88,14 @@ app.get("/posts/:title", function(req, res) {
 //////////////////////////////////////
 
 app.post("/compose", function(req, res) {
-  const post = {
+  const post = new Post({
     title: req.body.composeTitle,
     content: req.body.composeText
-  };
+  });
 
-  allPosts.push(post);
-  res.redirect("/");
+  post.save(function(err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
 });
