b6e6e96020bc36472785a3e5dac3d1c00c726aa4
Mohit Soni <mohitksoni@outlook.com>
2022-09-10 23:47:24 -0700

    Finished the React Module

---
+++ 18-React-Keeper-Website/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note(props) {
+  return (
+    <div className="note">
+      <h1>First Day!</h1>
+      <p>I hate doing this course sometimes.</p>
+    </div>
+  );
+}
+
+export default Note;
